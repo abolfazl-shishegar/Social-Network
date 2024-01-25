3493c1637f8c3835517843e32ae97ad4397aabce
aminsaberi52 <118849395+aminsaberi52@users.noreply.github.com>
2024-01-25 15:51:52 +0330

    step 3 & step 5 added

---
+++ _3.py
@@ -0,0 +1,49 @@
+﻿import pandas as pd
+#import nltk
+#nltk.download('punkt')
+#nltk.download('vader_lexicon')
+from nltk import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# خواندن دیتاست
+file_path = 'C:/Users/MOHAMMADAMIN/Desktop/sampeled2_gotTwitter.csv'
+df = pd.read_csv(file_path)
+
+# انتخاب فیلد متن
+text_field = 'text'
+texts = df[text_field]
+
+# توکن‌های متن را استخراج کنید
+all_words = []
+for text in texts.dropna():
+    all_words.extend(word_tokenize(text))
+
+# محاسبه فراوانی کلمات
+word_freq = FreqDist(all_words)
+
+# 25 مورد از کلمات کلیدی و پرکاربرد
+top_keywords = word_freq.most_common(25)
+print("Top 25 Keywords:")
+for keyword, freq in top_keywords:
+    print(f"{keyword}: {freq}")
+
+# تحلیل مثبت و منفی
+sia = SentimentIntensityAnalyzer()
+positive_count = 0
+negative_count = 0
+
+for text in texts.dropna():
+    sentiment_score = sia.polarity_scores(text)['compound']
+    if sentiment_score >= 0.05:
+        positive_count += 1
+    elif sentiment_score <= -0.05:
+        negative_count += 1
+
+total_texts = len(texts.dropna())
+positive_percentage = (positive_count / total_texts) * 100
+negative_percentage = (negative_count / total_texts) * 100
+
+print(f"\nPositive Percentage: {positive_percentage}%")
+print(f"Negative Percentage: {negative_percentage}%")
+
---
+++ _5_tag_mentions.py
@@ -0,0 +1,31 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+# خواندن داده‌ها از فایل CSV
+file_path = 'C:/Users/MOHAMMADAMIN/Desktop/sampeled2_gotTwitter.csv'
+df = pd.read_csv(file_path)
+
+# تحلیل جامعه‌های فعال بر اساس mentions_screen_name
+mentions_communities = df['mentions_screen_name'].str.split(',').explode().str.strip()
+mentions_active_communities = mentions_communities.value_counts()
+
+# تحلیل جامعه‌های فعال بر اساس hashtags
+hashtags_communities = df['hashtags'].str.split(',').explode().str.strip()
+hashtags_active_communities = hashtags_communities.value_counts()
+
+# ترکیب دو تحلیل بر اساس mentions و hashtags
+combined_active_communities = mentions_active_communities.add(hashtags_active_communities, fill_value=0)
+
+# نمایش ۱۰ جامعه فعال برتر
+top_communities = combined_active_communities.head(10)
+print("Top 10 Active Communities:")
+print(top_communities)
+
+# رسم نمودار توزیع جامعه‌های فعال
+plt.figure(figsize=(10, 6))
+top_communities.plot(kind='bar', color='skyblue')
+plt.title('Top 10 Active Communities')
+plt.xlabel('Screen Name or Hashtags')
+plt.ylabel('Number of Occurrences')
+plt.show()
+
