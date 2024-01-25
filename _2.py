9d486a828619b66304cc37b9b2f3ccc10e5ac62f
Abolfazl Shishegar <104524823+abolfazl-shishegar@users.noreply.github.com>
2024-01-25 15:56:27 +0330

    step 2 & 4 added

---
+++ _2.py
@@ -0,0 +1,55 @@
+﻿import pandas as pd
+import networkx as nx
+
+# خواندن فایل CSV
+file_path = 'C:/Users/Abolfazl/Downloads/Telegram Desktop/sampeled2_gotTwitter.csv'
+df = pd.read_csv(file_path)
+
+# ساخت گراف جهت‌دار
+G = nx.DiGraph()
+
+# افزودن یال‌ها به گراف بر اساس اطلاعات کاربران
+for index, row in df.iterrows():
+    source_user_id = row['user_id']
+    target_user_id = row['mentions_user_id']
+
+    if not pd.isnull(target_user_id):
+        G.add_edge(source_user_id, target_user_id)
+
+# محاسبه معیارهای اهمیت و مرکزیت
+degree_centrality = nx.degree_centrality(G)
+closeness_centrality = nx.closeness_centrality(G)
+betweenness_centrality = nx.betweenness_centrality(G)
+
+# گره‌های با اهمیت بالا (بر اساس معیارها)
+important_nodes_degree = sorted(
+    degree_centrality, key=degree_centrality.get, reverse=True)[:5]
+important_nodes_closeness = sorted(
+    closeness_centrality, key=closeness_centrality.get, reverse=True)[:5]
+important_nodes_betweenness = sorted(
+    betweenness_centrality, key=betweenness_centrality.get, reverse=True)[:5]
+
+# چاپ نتایج با اضافه کردن نام کاربر به کنار user_id
+print("Top 5 nodes based on Degree Centrality:")
+for node in important_nodes_degree:
+    user_id = node
+    name = df[df['user_id'] == user_id]['name'].values
+    if len(name) > 0:
+        name = name[0]
+        print(f"User ID: {user_id}, Name: {name}")
+
+print("\nTop 5 nodes based on Closeness Centrality:")
+for node in important_nodes_closeness:
+    user_id = node
+    name = df[df['user_id'] == user_id]['name'].values
+    if len(name) > 0:
+        name = name[0]
+        print(f"User ID: {user_id}, Name: {name}")
+
+print("\nTop 5 nodes based on Betweenness Centrality:")
+for node in important_nodes_betweenness:
+    user_id = node
+    name = df[df['user_id'] == user_id]['name'].values
+    if len(name) > 0:
+        name = name[0]
+        print(f"User ID: {user_id}, Name: {name}")
---
+++ _4.py
@@ -0,0 +1,33 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+# خواندن فایل CSV به عنوان DataFrame
+file_path = 'C:/Users/Abolfazl/Downloads/Telegram Desktop/sampeled2_gotTwitter.csv'
+df = pd.read_csv(file_path)
+
+# تحلیل تعداد فعالیت‌های هر کاربر
+user_activity = df['screen_name'].value_counts()
+
+# گرفتن 15 کاربر فعال
+top_users = user_activity.head(15)
+
+# نمایش نتایج
+print("لیست 15 کاربر فعال:")
+print(top_users)
+
+# تحلیل میزان تاثیرگذاری محتوای 15 کاربر بر کل شبکه
+total_tweets = df.shape[0]
+top_users_tweets = df[df['screen_name'].isin(top_users.index)].shape[0]
+impact_percentage = (top_users_tweets / total_tweets) * 100
+
+# نمایش نتایج
+print(
+    f"\nمیزان تاثیرگذاری محتوای 15 کاربر بر کل شبکه: {impact_percentage:.2f}%")
+
+# تحلیل و نمایش نمودار میزان فعالیت کاربران
+plt.figure(figsize=(12, 6))
+top_users.plot(kind='bar', color='skyblue')
+plt.title('Number of user activities')
+plt.xlabel('نام کاربری')
+plt.ylabel('Number of activities')
+plt.show()
